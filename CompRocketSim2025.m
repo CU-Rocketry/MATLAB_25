07adfc4280ed0643cbd9fcdac160a6ee567e352d
Sigmond Kukla <sigkukla@gmail.com>
2025-02-07 17:57:10 -0500

    Add drag curve graph

--- CompRocketSim2025.m
+++ CompRocketSim2025.m
@@ -10,7 +10,14 @@ motor_dry_mass = motor_wet_mass - motor_prop_mass;
 g = 9.81;                % [m/s^2] Gravity
 %% Functions
 % Drag Function
+drag_curve = readmatrix("drag_curve.csv");
+drag_curve_mach = drag_curve(:,1);
+drag_curve_drag = drag_curve(:,2);
 
+figure;
+plot(drag_curve_mach, drag_curve_drag);
+xlabel("Mach number");
+ylabel("Maybe the Cd??")
 
 % Motor Function
 
