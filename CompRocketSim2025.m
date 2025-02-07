c9498d2b7358071bfdc6637605e1d010619a1d72
8glaney5 <laneylougoldman@gmail.com>
2025-02-07 17:41:15 -0500

    Added motor variables

--- CompRocketSim2025.m
+++ CompRocketSim2025.m
@@ -2,6 +2,12 @@
 
 %% Variables
 
+motor_fname = "AeroTech_M2500.eng"; %'thrust_curves/AeroTech_M2500T.rse'
+motor_wet_mass = 8.108;  % [kg] Mass with no fuel
+motor_prop_mass = 4.766; % [kg] Mass of prop
+motor_dry_mass = motor_wet_mass - motor_prop_mass;
+
+g = 9.81;                % [m/s^2] Gravity
 %% Functions
 % Drag Function
 
