aa1c6c9faa9b95f85ab40092ed65190c608ccc77
Elizabeth <eswartz2023@gmail.com>
2025-02-24 18:37:48 -0500

    Corrected Drag Function y-label

--- CompRocketSim2025.m
+++ CompRocketSim2025.m
@@ -2,12 +2,12 @@
 
 %% Variables
 
-motor_fname = "AeroTech_M2500.eng"; %'thrust_curves/AeroTech_M2500T.rse'
-motor_wet_mass = 8.108;  % [kg] Mass with no fuel
-motor_prop_mass = 4.766; % [kg] Mass of prop
+motor_fname = "AeroTech_M2500.eng";     %'thrust_curves/AeroTech_M2500T.rse'
+motor_wet_mass = 8.108;                 % [kg] Mass with no fuel
+motor_prop_mass = 4.766;                % [kg] Mass of prop
 motor_dry_mass = motor_wet_mass - motor_prop_mass;
 
-g = 9.81;                % [m/s^2] Gravity
+g = 9.81;                               % [m/s^2] Gravity
 %% Functions
 % Drag Function
 drag_curve = readmatrix("drag_curve.csv");
@@ -17,7 +17,7 @@ drag_curve_drag = drag_curve(:,2);
 figure;
 plot(drag_curve_mach, drag_curve_drag);
 xlabel("Mach number");
-ylabel("Maybe the Cd??")
+ylabel("Drag Coefficient")
 
 % Motor Function
 
